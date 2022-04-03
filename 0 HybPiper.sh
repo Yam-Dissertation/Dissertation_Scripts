75fb85cf856f3d3400d8fcca33d9b98d28513da4
Yam-Dissertation <101579273+Yam-Dissertation@users.noreply.github.com>
2022-04-03 16:19:04 +0100

    Annotation and clarification

--- 0 HybPiper.sh
+++ 0 HybPiper.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-for file in *.fastq;
+# Run this file from the directory containing paired end sequence output.
+# It assumes that the HybPiper python scripts are stored in ../Packages/HybPiper/
+
+for file in *.fastq; # Loops through all paired end sequence files in the current directory
 do
 echo "${file%.*}"
-done > namelist.txt
+done > namelist.txt # Writes the file prefixes to a list
 
-while read name;
+while read name; # For each of the file prefixes in that list
 do
-full="${name}".fastq
-python ../Packages/HybPiper/reads_first.py --cpu 1 -b test_targets.fasta -r $full --prefix $name --bwa
-#python ../Packages/HybPiper/cleanup.py $name
+full="${name}".fastq # The full filename is stored
+python ../Packages/HybPiper/reads_first.py --cpu 1 -b target_genes.fasta -r $full --prefix $name --bwa # Gene extraction of the target genes is undertaken in that file
+python ../Packages/HybPiper/cleanup.py $name # Optional line to clean up the many files that HybPiper produces
 done < namelist.txt
 
-#hybpiper get_seq_lengths test_targets.fasta namelist.txt dna > test_seq_lengths.txt
+python ../Packages/HybPiper/get_seq_lengths.py test_targets.fasta namelist.txt dna > test_seq_lengths.txt # The gene recovery percentages which are used to identify extraction failure
 
 # In order to view these plots, need to open files in RStudio with gplots and heatmap.plus packages installed
 
-hybpiper stats test_seq_lengths.txt namelist.txt > test_stats.txt
+python ../Packages/HybPiper/stats.py test_seq_lengths.txt namelist.txt > test_stats.txt # Optional line to produce series of summary statistics about the data
 
-hybpiper retrieve_sequences test/targets.fasta . dna
+python ../Packages/HybPiper/retrieve_sequences.py test/targets.fasta . dna # Retrieves the sequences for the same gene for many samples and generates an unaligned multi-FASTA file
 
+# This FASTA file becomes the input for the next stage.
