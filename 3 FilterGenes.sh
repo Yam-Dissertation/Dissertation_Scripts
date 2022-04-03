60ef8797cc2d2d82597f1072a8997e1a4e4f4fa2
Yam-Dissertation <101579273+Yam-Dissertation@users.noreply.github.com>
2022-04-03 16:29:54 +0100

    Annotation and Clarification

--- 3 FilterGenes.sh
+++ 3 FilterGenes.sh
@@ -1,16 +1,16 @@
-# RUN THIS FROM THE FOLDER CONTAINING FNA FILES
+# This should be run from the directory containing the Hybpiper outputs in FNA format and the subset files.
 
 for recoveryfile in *PercentRecovery*
 do
-Tempname="${recoveryfile%PercentRecovery*}"
+Tempname="${recoveryfile%PercentRecovery*}" # For each phylogeny
 Newfolder="${Tempname}TreeFolder"
-GenesFilter="${Tempname}SubsettedGenes.txt"
-SpeciesFilter="${Tempname}SubsettedSpecies.txt"
+GenesFilter="${Tempname}SubsettedGenes.txt" # The name of the gene subset file for that tree
 
 dos2unix $GenesFilter
-mkdir ../$Newfolder
-while read -r gene;
+mkdir ../$Newfolder # Make a new folder for that phylogeny
+while read -r gene; # For each gene in that gene subset file
 do
-cp "${gene}.FNA" ../$Newfolder/
+cp "${gene}.FNA" ../$Newfolder/ # Copy the corresponding FNA file to the new folder
 done < $GenesFilter
+
 done
