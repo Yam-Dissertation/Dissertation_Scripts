0c6c6d01e8057f26ea03339ad4b7532b25e48916
Yam-Dissertation <101579273+Yam-Dissertation@users.noreply.github.com>
2022-04-03 19:07:55 +0100

    Annotation and Clarification

--- 8 Score Cutoff.R
+++ 8 Score Cutoff.R
@@ -1,9 +1,8 @@
 ScoreCutoff <- function(RankedScores){
-  ScoreData <- read.csv(RankedScores, header = T)
+  ScoreData <- read.csv(RankedScores, header = T) # Read in a table of Ranked Scores
   Scores <- ScoreData$Score
-  Cutoff <- mean(Scores) + (2*sd(Scores))
-  print(ScoreData[ScoreData$Score > Cutoff,])
-  print(paste0("Num Prio Species: ", length(unique(ScoreData$Priority.Species[ScoreData$Score > Cutoff]))))
+  Cutoff <- mean(Scores) + (2*sd(Scores)) # Calculate a cutoff value
+  print(ScoreData[ScoreData$Score > Cutoff,]) # Print species pairs whose distance score is greater than the cutoff value.
+  print(paste0("Num Prio Species: ", length(unique(ScoreData$Priority.Species[ScoreData$Score > Cutoff])))) # Print the number of priority species
   return(Cutoff)
 }
-ScoreCutoff("C:\\Users\\joshu\\OneDrive\\Josh\\BIOL0019\\Working Directory\\FinalRun\\FullConcatTreeFolder\\RankScoresEnantiophyllum.csv")
