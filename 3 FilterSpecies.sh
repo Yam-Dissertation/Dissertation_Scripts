d0a355a1aea11c900b5360acfdcef8a29399df63
Yam-Dissertation <101579273+Yam-Dissertation@users.noreply.github.com>
2022-04-03 16:33:34 +0100

    Annotation and Clarification

--- 3 FilterSpecies.sh
+++ 3 FilterSpecies.sh
@@ -1,25 +1,26 @@
-# RUN THIS FROM THE FOLDER CONTAINING FNA FILES
+# This should be run from the directory containing the Hybpiper outputs in FNA format and the subset files.
 
 for recoveryfile in *PercentRecovery*
 do
-Tempname="${recoveryfile%PercentRecovery*}"
+Tempname="${recoveryfile%PercentRecovery*}" # For each Treefolder
 Folder="${Tempname}TreeFolder"
 SamplesFilter="${Tempname}SubsettedSpecies.txt"
-GenesFilter="${Tempname}SubsettedGenes.txt"
 
 dos2unix $SamplesFilter
 
-for file in ../$Folder/*.FNA;
+for file in ../$Folder/*.FNA; # For each FNA file
 do
 awk '{ if (NR==1) { printf("%s", $0); } else if ((NR>1)&&($0~/^>/)) { printf("\n%s", $0); } else {printf("\t%s", $0); } }' $file > "${file%.*}FIRST.FNA" # Firstly linearise the sequences
 grep -Ff $SamplesFilter "${file%.*}FIRST.FNA" > "${file%.*}SECOND.FNA" # Then keep only those matching in the file
 tr "\t" "\n" < "${file%.*}SECOND.FNA" > "${file%.*}SUBSETTED.FNA" # Then delinearise the sequences
 done
 
+# Remove intermediate and outdated files
 rm -r ../$Folder/*FIRST*
 rm -r ../$Folder/*SECOND*
 find ../$Folder/ -type f ! -name '*SUBSETTED.FNA' -delete
 
+# Copy a number of useful files into the tree folder (if they are modified elsewhere later, these files will reflect their state at the time of folder creation)
 cp NameMatchFinal.csv ../$Folder/
 cp $recoveryfile ../$Folder/
 cp -r Final\ Scripts ../$Folder/
