de732d060832b47daa69fd190b3fc553b37c5e64
Yam-Dissertation <101579273+Yam-Dissertation@users.noreply.github.com>
2022-04-03 16:58:10 +0100

    Annotation and Clarification

--- 5 ConcatIQTree.sh
+++ 5 ConcatIQTree.sh
@@ -1,39 +1,42 @@
-# RUN THIS FROM THE SUBSETTED FOLDER
+# This should be run from the directory containing the Hybpiper outputs in FNA format and the subset files.
 
-for recoveryfile in *PercentRecovery*
+for recoveryfile in *PercentRecovery* # For each phylogeny
 do
 
 Tempname="${recoveryfile%PercentRecovery*}"
 Folder="${Tempname}TreeFolder"
 ConcatOutput="${Tempname}Concat.fas"
 
-FastaCount=$(ls -lR ../$Folder/*.fasta | wc -l)
+FastaCount=$(ls -lR ../$Folder/*.fasta | wc -l) # Count the number of FASTA files in that folder
 cd ../$Folder/
-if [ $FastaCount -eq 1 ];
+if [ $FastaCount -eq 1 ]; # If there is only one FASTA file (i.e. it's a single gene phylogeny)
 then
 
-iqtree -s "${Tempname}TRIMAL.fasta" -alrt 1000 -bb 1000 -nt AUTO
+iqtree -s "${Tempname}TRIMAL.fasta" -alrt 1000 -bb 1000 -nt AUTO # Run IQ-Tree
 
-else
+else # If there are more than one FASTA file (i.e. it's a functionally grouped tree or a full GOI/LCN tree)
 mkdir TempForConcat
 
-for file in *TRIMAL.fasta;
+for file in *TRIMAL.fasta; # Copy all FASTA files into a new folder
 do
 cp $file TempForConcat
 done
 
-cd TempForConcat
+cd TempForConcat # Enter that folder
 
-perl ../../../../Packages\ and\ Software/FASconCAT-G-master/FASconCAT-G-master/FASconCAT-G_v1.05.pl -s
+perl ../../../../Packages\ and\ Software/FASconCAT-G-master/FASconCAT-G-master/FASconCAT-G_v1.05.pl -s # Concatenate them using FASconCAT-G
 
-mv FcC_info.xls ../"${Tempname}Concat.xls"
+mv FcC_info.xls ../"${Tempname}Concat.xls" # Move the concatenated file back to the original folder
 mv FcC_supermatrix.fas ../$ConcatOutput
 
-cd ../
+cd ../ # Return to the original folder
 
-iqtree -s $ConcatOutput -alrt 1000 -bb 1000 -nt AUTO
+iqtree -s $ConcatOutput -alrt 1000 -bb 1000 -nt AUTO # Run IQ-Tree
 
-rm -r TempForConcat
+rm -r TempForConcat # Remove the temporary concatination folder
 fi
 
 done
+
+# Note: This will take a very long time, especially if you have lots of species and or genes.
+# For 39 genes and 333 species this took 36 hours.
