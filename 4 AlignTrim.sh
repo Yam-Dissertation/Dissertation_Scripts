30e77512bdd9315523fdb023fdd1e3ed502f5721
Yam-Dissertation <101579273+Yam-Dissertation@users.noreply.github.com>
2022-04-03 16:35:10 +0100

    Annotation and Clarification

--- 4 AlignTrim.sh
+++ 4 AlignTrim.sh
@@ -1,15 +1,18 @@
-# RUN THIS FOLDER CONTAINING FNAS
-for recoveryfile in *PercentRecovery*
+# This should be run from the directory containing the Hybpiper outputs in FNA format and the subset files.
+
+for recoveryfile in *PercentRecovery* # For each treefolder
 do
 Tempname="${recoveryfile%PercentRecovery*}"
 Folder="${Tempname}TreeFolder"
 
-for file in ../$Folder/*.FNA;
+for file in ../$Folder/*.FNA; # For each FNA file in that folder
 do
-# Align
+# Align all sequences in that FNA file
 mafft --auto $file > "${file%SUBSETTED*}MAFFT.FNA"
-# Trim
+# Trim gaps which contain no information but increase time taken to construct phylogenies
 trimal -in "${file%SUBSETTED*}MAFFT.FNA" -out "${file%SUBSETTED*}TRIMAL.fasta" -automated1
 done
 
 done
+
+# The output will be a series of files ending TRIMAL.fasta
