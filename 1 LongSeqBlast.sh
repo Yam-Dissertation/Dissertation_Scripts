c42cf993050e85de1028a6074b56c2c48bab9c5e
Yam-Dissertation <101579273+Yam-Dissertation@users.noreply.github.com>
2022-04-03 16:22:45 +0100

    Annotation and Clarification

--- 1 LongSeqBlast.sh
+++ 1 LongSeqBlast.sh
@@ -1,16 +1,18 @@
-mkdir ../BLAST
+# Run this from the folder containing the HybPiper output files in FNA format
 
-# Copy fna files to a new folder
+# Copy fna files to a new folder called BLAST
+mkdir ../BLAST
 for fna in *.FNA;
 do
 cp $fna ../BLAST
 done
 
+# Navigate to that folder
 cd ../BLAST
 
 
 # Linearise the sequence, add a column for length of sequence, sort by that new column, remove that column, and delinearise.
-for file in *.FNA;
+for file in *.FNA; # For each of the FNA files
 do
 awk '/^>/ {if(N>0) printf("\n"); printf("%s\t",$0);N++;next;} {printf("%s",$0);} END {if(N>0) printf("\n");}' $file |\
 awk -F '	'  '{printf("%s\t%d\n",$0,length($2));}' |\
@@ -21,7 +23,10 @@ fold -w 60 |\
 awk '/^>/{if(N)exit;++N;} {print;}' > "${file%.*}Longest.fasta"
 done
 
+# Remove the original FNA file (so that only the FASTA files sorted by sequence length remain).
 for file in ../BLAST/*.FNA;
 do
 rm $file
 done
+
+# For each of the FNA files (one for each gene), the first sequence in the FASTA file will be the longest and should be used for BLAST.
